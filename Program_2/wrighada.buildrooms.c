a369fdfce09b89848540c19eec1e6445e7d52d6c
Adam Wright <adam@adamjwright.com>
2020-04-27 01:09:42 -0700

    fix gcc warnings

--- Program_2/wrighada.buildrooms.c
+++ Program_2/wrighada.buildrooms.c
@@ -13,6 +13,7 @@
 #include <string.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <unistd.h>
 #include <time.h>
 
 
@@ -224,6 +225,7 @@ bool IsGraphFull(struct Room* room_arr[])
             i++;
         }
     }
+    return true;
 }
 
 
