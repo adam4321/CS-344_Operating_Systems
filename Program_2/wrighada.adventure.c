a3471cbdb70e7b790e6d2b2d20ee9355d374a8b6
Adam Wright <adam@adamjwright.com>
2020-04-26 23:41:39 -0700

    find the newest directory

--- Program_2/wrighada.adventure.c
+++ Program_2/wrighada.adventure.c
@@ -14,6 +14,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <time.h>
+#include <dirent.h>
 
 
 /* Global Constants */
@@ -36,6 +37,9 @@ struct Room
     struct Room* out_connect[CONNECT_MAX];
 };
 
+/* Array to hold the created rooms */
+struct Room* Room_Arr[ROOM_COUNT];
+
 /* Array holding the 3 types of the rooms */
 char* room_types[3] = 
 {
@@ -44,18 +48,18 @@ char* room_types[3] =
     "MID_ROOM"
 };
 
+
 /* FUNCTION DECLARATIONS ----------------------------------------------------*/
 
-
+void Get_Newest_Dir();
+void Game_Prompt();
 
 
 /* MAIN ---------------------------------------------------------------------*/
 
 int main()
 {
-    printf("CURRENT LOCATION: \n");
-    printf("POSSIBLE CONNECTIONS: \n");
-    printf("WHERE TO? >");
+    Get_Newest_Dir();
     
 
     return 0;
@@ -63,5 +67,54 @@ int main()
 
 
 /* FUNCTION DEFINITIONS -----------------------------------------------------*/
+
+/* Function that finds the most recent room directory */
+void Get_Newest_Dir()
+{
+  int newestDirTime = -1; // Modified timestamp of newest subdir examined
+  char targetDirPrefix[32] = "wrighada.rooms."; // Prefix we're looking for
+  char newestDirName[256]; // Holds the name of the newest dir that contains prefix
+  memset(newestDirName, '\0', sizeof(newestDirName));
+
+  DIR* dirToCheck; // Holds the directory we're starting in
+  struct dirent *fileInDir; // Holds the current subdir of the starting dir
+  struct stat dirAttributes; // Holds information we've gained about subdir
+
+  dirToCheck = opendir("."); // Open up the directory this program was run in
+
+  if (dirToCheck > 0) // Make sure the current directory could be opened
+  {
+    while ((fileInDir = readdir(dirToCheck)) != NULL) // Check each entry in dir
+    {
+      if (strstr(fileInDir->d_name, targetDirPrefix) != NULL) // If entry has prefix
+      {
+        printf("Found the prefex: %s\n", fileInDir->d_name);
+        stat(fileInDir->d_name, &dirAttributes); // Get attributes of the entry
+
+        if ((int)dirAttributes.st_mtime > newestDirTime) // If this time is bigger
+        {
+          newestDirTime = (int)dirAttributes.st_mtime;
+          memset(newestDirName, '\0', sizeof(newestDirName));
+          strcpy(newestDirName, fileInDir->d_name);
+          printf("Newer subdir: %s, new time: %d\n",
+                 fileInDir->d_name, newestDirTime);
+        }
+      }
+    }
+  }
+
+  closedir(dirToCheck); // Close the directory we opened
+
+  printf("Newest entry found is: %s\n", newestDirName);
+}
+
+/* Print the game menu */
+void Game_Prompt()
+{
+    printf("CURRENT LOCATION: \n");
+    printf("POSSIBLE CONNECTIONS: \n");
+    printf("WHERE TO? >");
+}
+
 
 
