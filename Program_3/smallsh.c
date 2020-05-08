9574f0c18755aef147c565363e2c19bb03fc7e1e
Adam Wright <adam@adamjwright.com>
2020-05-07 23:01:06 -0700

    add skeleton file

---
+++ Program_3/smallsh.c
@@ -0,0 +1,28 @@
+/******************************************************************************
+**  Author:      Adam Wright
+**  Email:       wrighada@oregonstate.edu
+**  Date:        5/6/2020
+**  Description: Program 3 for spring 2020 OSU cs-344. This program implements
+**               a basic bare-bones shell for Unix.
+******************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/* FUNCTION DECLARATIONS ----------------------------------------------------*/
+
+
+/* MAIN ---------------------------------------------------------------------*/
+
+int main()
+{
+
+
+    return 0;
+}
+
+
+/* FUNCTION DEFINITIONS -----------------------------------------------------*/
+
+
