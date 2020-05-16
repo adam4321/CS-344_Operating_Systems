f7bbf7ddead5be6c00bc61f7d77f93fc46359b32
Adam Wright <adam@adamjwright.com>
2020-05-15 23:06:15 -0700

    building out functions

--- Program_3/smallsh.c
+++ Program_3/smallsh.c
@@ -12,22 +12,32 @@
 #include <stdbool.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <signal.h>
 
 
 #define BUF_SIZE    2049    // Maximum number of characters supported
-#define ARG_CNT     513     // Maximum number of commands supported
+#define MAX_ARGS     512    // Maximum number of commands supported
+
+bool is_fg_only = false;
 
 
 /* FUNCTION DECLARATIONS ----------------------------------------------------*/
 
-void get_status(int status);
+void catchSIGINT(int signo);
 void change_dir(char **arg_arr);
+void get_status(int status);
 
 
 /* MAIN ---------------------------------------------------------------------*/
 
 int main()
 {
+    char *input_buf = malloc(BUF_SIZE);     // String for CLI input
+    char *arg_arr[MAX_ARGS];                // Array of strings to hold CLI arguments
+    char *in_file = NULL;                   // Input file pointer
+    char *out_file = NULL;                  // Output file pointer
+
+
     
     // Start the shell and keep it running
     while (true)
@@ -35,6 +45,22 @@ int main()
         printf(": ");
         fflush(stdout);
         fflush(stderr);
+
+        // Skip any comments or blank lines
+        if (strncmp(input_buf, "#", 1) == 0 || strncmp(input_buf, "\0", 1) == 0)
+        {
+            continue;
+        }
+
+        // Check for and exit command
+        if (strcmp(arg_arr[0], "exit") == 0)
+        {
+            // Free memory
+
+
+            exit(EXIT_SUCCESS);
+        }
+
 
 
     }
@@ -45,18 +71,26 @@ int main()
 
 /* FUNCTION DEFINITIONS -----------------------------------------------------*/
 
+void catchSIGINT(int signo)
+{
+  char* message = "SIGINT. Use CTRL-Z to Stop.\n";
+  write(STDOUT_FILENO, message, 28);
+}
+
+
 // Print the exit value or the terminating signal
 void get_status(int status)
 {
+    // Prints the exit status of the last foreground process
     if (WIFEXITED(status))
     {
         printf("exit value %i\n", WEXITSTATUS(status));
     }
+    // Prints the terminating signal of the last foreground process
     else
     {
         printf("terminated by signal %i", status);
     }
-    
 }
 
 
