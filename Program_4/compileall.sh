ece8bb55b41e23da4b7f9a2ded8bf1482b622432
Adam Wright <adam@adamjwright.com>
2020-05-27 23:13:25 -0700

    add compile script skeleton

---
+++ Program_4/compileall.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+###############################################################################
+##  Author:       Adam Wright
+##  Email:        wrighada@oregonstate.edu
+##  Date:         5-23-2020
+##  Description:  Bash shell script to compile program 4 for OSU cs344 Spring
+##                2020.
+###############################################################################
+
