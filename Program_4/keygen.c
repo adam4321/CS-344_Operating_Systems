703f01f650e07480d665f5ea267a18962ef7522b
Adam Wright <adam@adamjwright.com>
2020-05-28 14:35:33 -0700

    complete compile script

--- Program_4/keygen.c
+++ Program_4/keygen.c
@@ -8,20 +8,36 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <time.h>
 
 
-/* FUNCTION DECLARATIONS ----------------------------------------------------*/
+const char *CHAR_SET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ ";
 
 
 /* MAIN ---------------------------------------------------------------------*/
 
-int main()
+int main(int argc, char **argv)
 {
+    // Check for the correct number of arguments
+    if (argc != 2)
+    {
+        fprintf(stderr, "Error: incorrect argument count to %s\n", argv[1]);
+        exit(1);
+    }
 
+    int input_length = atoi(argv[1]);
+    char user_input[input_length + 1];
+
+    // Seed the random function
+    srand(time(0));
+
+    // Generate the key string from the character set
+    int i = 0;
+    while (i < input_length)
+    {
+        
+    }
 
     return 0;
 }
-
-/* FUNCTION DEFINITIONS -----------------------------------------------------*/
-
-
