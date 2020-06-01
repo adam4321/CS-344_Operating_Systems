9bdd825532ca6d6fd6df37ef8f5b25bed7b1b77e
Adam Wright <adam@adamjwright.com>
2020-06-01 04:26:24 -0700

    post message string built

--- Program_4/otp.c
+++ Program_4/otp.c
@@ -23,6 +23,7 @@
 #include <netdb.h>
 
 #define CHAR_SET_SIZE   27
+#define MAX_BUFFER_SIZE 80000
 
 const char *CHAR_SET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ ";
 
@@ -30,9 +31,9 @@ const char *CHAR_SET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ ";
 /* FUNCTION DECLARATIONS -----------------------------------------------------*/
 
 void error(const char *msg);
-int file_test(FILE *infile);
-void encrypt_msg(char *msg, char *plain_text, char *key);
-void decrypt_msg(char *msg, char *plain_text, char *key);
+int file_read_test(char *name, char *buffer, int size);
+void encrypt_msg(char *msg_str, char *plaintext_str, char *key_str);
+void decrypt_msg(char *msg_str, char *cipher_str, char *key_str);
 
 
 /* MAIN ---------------------------------------------------------------------*/
@@ -42,7 +43,7 @@ int main(int argc, char *argv[])
 	int socketFD, portNumber, charsWritten, charsRead;
 	struct sockaddr_in serverAddress;
 	struct hostent* serverHostInfo;
-	char buffer[256];
+	char buffer[MAX_BUFFER_SIZE];
     
     // Check usage & args
 	if (argc == 5)
@@ -77,7 +78,7 @@ int main(int argc, char *argv[])
         exit(2);
     }
 
-
+    // CLIENT GET MODE
     if (strcmp(argv[1], "get") == 0)
     {
 
@@ -115,44 +116,23 @@ int main(int argc, char *argv[])
     }
 
 
+    // CLIENT POST MODE
     if (strcmp(argv[1], "post") == 0)
     {
-        memset(buffer, '\0', sizeof(buffer)); // Clear out the buffer array
-        strcat(buffer, argv[1]);
-        strcat(buffer, " ");
-        strcat(buffer, argv[2]);
-        strcat(buffer, " ");
-
-        FILE *fptr1 = fopen(argv[3], "r");
-        FILE *fptr2 = fopen(argv[4], "r");
-        printf("%s %s\n", argv[3], argv[4]);
-        long plaintext_len;
-        long key_len;
-
-        if (!fptr1 || !fptr2)
-        {
-            fprintf(stderr, "Input file cannot be opened\n");
-            exit(1);
-        }
+        // Clear out the buffer array
+        memset(buffer, '\0', sizeof(buffer));
         
-        plaintext_len = file_test(fptr1);
-        if (plaintext_len == -1)
-        {
-            fprintf(stderr, "Bad characters in %s\n", argv[3]);
-            exit(1);
-        }
-
-        key_len = file_test(fptr2);
-        if (plaintext_len == -1)
-        {
-            fprintf(stderr, "Bad characters in %s\n", argv[4]);
-            exit(1);
-        }
+        // Alocate memory for the 3 strings
+        char plaintext_str[MAX_BUFFER_SIZE];
+        char key_str[MAX_BUFFER_SIZE];
+        char msg_str[MAX_BUFFER_SIZE];
+        memset(msg_str, '\0', sizeof(MAX_BUFFER_SIZE));
         
+        // Read in the files and test their contents for bad characters
+        long plaintext_len = file_read_test(argv[3], plaintext_str, MAX_BUFFER_SIZE);
+        long key_len = file_read_test(argv[4], key_str, MAX_BUFFER_SIZE);
 
-        // char *input_msg = malloc()
-        
-
+        // Test that the key file is equal of longer than plaintext 
         if (plaintext_len > key_len)
         {
             fprintf(stderr, "The key file must be equal or longer than plaintext\n");
@@ -160,8 +140,26 @@ int main(int argc, char *argv[])
         }
 
 
-        fclose(fptr1);
-        fclose(fptr2);
+        printf("%s\n", key_str);
+        printf("%s\n", plaintext_str);
+        
+
+        // Call the function to encrypt the plaintext string
+        encrypt_msg(msg_str, plaintext_str, key_str);
+        printf("%s\n", msg_str);
+
+        char new_str[MAX_BUFFER_SIZE];
+        memset(new_str, '\0', sizeof(MAX_BUFFER_SIZE));
+        decrypt_msg(new_str, msg_str, key_str);
+        printf("%s\n", new_str);
+
+
+        // Assemble the message string
+        strcat(buffer, argv[1]);
+        strcat(buffer, " ");
+        strcat(buffer, argv[2]);
+        strcat(buffer, " ");
+        strcat(buffer, msg_str);
 
         // Set up the server address struct
         memset((char*)&serverAddress, '\0', sizeof(serverAddress)); // Clear out the address struct
@@ -216,57 +214,71 @@ void error(const char *msg)
 
 
 // Check the size of the passed in files
-int file_test(FILE *infile)
-{
-    int cur_char;
-    int count = 0;
-    
-    cur_char = fgetc(infile);
+int file_read_test(char *name, char *buffer, int size)
+{  
+    FILE *fptr = fopen(name, "r");
+    int i;
 
-    while (cur_char != EOF && cur_char != '\n')
+    // Check that the input files are open
+    if (!fptr)
     {
-        if (!isupper(cur_char) || cur_char != ' ')
-        {
-            return -2;
-        }
-
-        cur_char = fgetc(infile);
-        count++;
+        fprintf(stderr, "%s cannot be opened\n", name);
+        exit(1);
     }
 
-    return count;
+    // Clear the buffer and read in the file contents
+    memset(buffer, '\0', size);
+    fgets(buffer, size - 1, fptr);
+    buffer[strcspn(buffer, "\n")] = '\0';
+    int length = strlen(buffer);
+
+    // Test the string for bad characters
+    for (i = 0; i < length; i++)
+    {
+        if (!isupper(buffer[i]) && buffer[i] != ' ') {
+            fprintf(stderr, "File \"%s\" contains bad characters\n", name);
+            fclose(fptr);
+            exit(1);
+        }
+    }
+
+    // Close the file and return the string length
+    fclose(fptr);
+    return length;
 }
 
 
 // Function to encrypt a post mode message
-void encrypt_msg(char *msg, char *plain_text, char *key)
+void encrypt_msg(char *msg_str, char *plaintext_str, char *key_str)
 {
-    int plain_text_len = strlen(plain_text);
-    int key_len = strlen(key);
+    int plain_text_len = strlen(plaintext_str);
+    int key_len = strlen(key_str);
 
     int i;
     for (i = 0; i < plain_text_len; i++)
     {
-        int msg_idx = strchr(CHAR_SET, plain_text[i]) - CHAR_SET;
-        msg_idx += strchr(CHAR_SET, key[i]) - CHAR_SET;
+        int msg_idx = strchr(CHAR_SET, plaintext_str[i]) - CHAR_SET;
+        msg_idx += strchr(CHAR_SET, key_str[i]) - CHAR_SET;
         msg_idx %= CHAR_SET_SIZE;
 
-        msg[i] = CHAR_SET[msg_idx];
+        msg_str[i] = CHAR_SET[msg_idx];
     }
+
+    msg_str[i] = '\0';
 }
 
 
 // Function to decrypt a get mode message
-void decrypt_msg(char *msg, char *plain_text, char *key)
+void decrypt_msg(char *msg_str, char *cipher_str, char *key_str)
 {
-    int msg_len = strlen(msg);
-    int key_len = strlen(key);
+    int cipher_len = strlen(cipher_str);
+    int key_len = strlen(key_str);
 
     int i;
-    for (i = 0; i < msg_len; i++)
+    for (i = 0; i < cipher_len; i++)
     {
-        int msg_idx = strchr(CHAR_SET, msg[i]) - CHAR_SET;
-        msg_idx += strchr(CHAR_SET, key[i]) - CHAR_SET;
+        int msg_idx = strchr(CHAR_SET, cipher_str[i]) - CHAR_SET;
+        msg_idx -= (strchr(CHAR_SET, key_str[i]) - CHAR_SET);
         if (msg_idx < 0)
         {
             msg_idx += CHAR_SET_SIZE;
@@ -276,6 +288,8 @@ void decrypt_msg(char *msg, char *plain_text, char *key)
             msg_idx %= CHAR_SET_SIZE;
         }
         
-        plain_text[i] = CHAR_SET[msg_idx];
+        msg_str[i] = CHAR_SET[msg_idx];
     }
+
+    msg_str[i] = '\0';
 }
